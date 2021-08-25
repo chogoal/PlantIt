8975d4df6ec72cb0783a84343f7d3f8347746c84
kovus380 <kosaf888@gmail.com>
2021-08-25 21:47:45 +0900

    delete kk

--- shopapp/models.py
+++ shopapp/models.py
@@ -3,8 +3,11 @@ from django.db import models
 
 
 # Create your models here.
+from backgroundapp.models import Background
 
 
 class Purchasing(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE,
                              related_name='shop', null=False)
+    background = models.ForeignKey(Background, on_delete=models.CASCADE,
+                                   related_name='shop', null=False)
