ffe719b538b1b64d1e6ad78cdb2fffd90bc426ff
kovus380 <kosaf888@gmail.com>
2021-08-27 11:36:42 +0900

    shopapp update

--- backgroundapp/models.py
+++ backgroundapp/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
@@ -9,3 +10,13 @@ class Background(models.Model):
     image = models.ImageField(upload_to='background/', null=False)
     price = models.IntegerField(default=15)
     state = models.CharField(max_length=10, default='unsold')
+
+#
+# class Purchasing(models.Model):
+#     user = models.ForeignKey(User, on_delete=models.CASCADE,
+#                              related_name='shop', null=False)
+#     background = models.ForeignKey(Background, on_delete=models.CASCADE,
+#                                    related_name='shop', null=False)
+#
+#     class Meta:
+#         unique_together = ['user', 'background']
--- shopapp/urls.py
+++ shopapp/urls.py
@@ -1,3 +1,9 @@
-urlpatterns = [
+from django.urls import path
 
+from shopapp.views import PurchasingView
+
+app_name = 'shopapp'
+
+urlpatterns = [
+    path('background/<int:background_pk>', PurchasingView.as_view(), name='backgroundpurchasing')
 ]
