658a2f1a2e3f1e2ae50fbed6149d352c0fd68aee
kovus380 <kosaf888@gmail.com>
2021-09-24 16:49:17 +0900

    login signup using bootstrap form

--- accountapp/forms.py
+++ accountapp/forms.py
@@ -5,5 +5,4 @@ from django.contrib.auth.forms import UserCreationForm
 class AccountCreationForm(UserCreationForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-
         self.fields['username'].disabled = True
