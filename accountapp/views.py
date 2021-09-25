ce11d3d2a23a6f857a5a334ba6c68b8da71c933e
kovus380 <kosaf888@gmail.com>
2021-09-26 05:20:23 +0900

    detail

--- accountapp/views.py
+++ accountapp/views.py
@@ -20,7 +20,7 @@ class AccountCreateView(CreateView):
     template_name = 'accountapp/create.html'
 
     def get_success_url(self):
-        return reverse('accountapp:detail', kwargs={'pk': self.object.pk})
+        return reverse('accountapp:login')
 
 
 class AccountDetailView(DetailView):
